c923140fe9e9f4785d6de1f169f76654534f8a10
Warvin B Mogate <80726319+vin0414@users.noreply.github.com>
2025-05-04 21:23:20 +0800

    update forms

--- app/Views/admin/register-cadet.php
+++ app/Views/admin/register-cadet.php
@@ -108,6 +108,238 @@
                                                 </div>
                                             </div>
                                         </div>
+                                        <div class="col-lg-12">
+                                            <label class="form-label">Student No</label>
+                                            <input type="text" class="form-control" name="student_no"
+                                                placeholder="Student No" required />
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <div class="row g-3">
+                                                <div class="col-lg-3">
+                                                    <label class="form-label">House No</label>
+                                                    <input type="text" class="form-control" name="house_no"
+                                                        placeholder="House No" required>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                    <label class="form-label">Street</label>
+                                                    <input type="text" class="form-control" name="street"
+                                                        placeholder="Street" required>
+                                                </div>
+                                                <div class="col-lg-6">
+                                                    <label class="form-label">Barangay/Subdivision</label>
+                                                    <input type="text" class="form-control" name="barangay"
+                                                        placeholder="Barangay/Subdivision" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <div class="row g-3">
+                                                <div class="col-lg-6">
+                                                    <label class="form-label">City/Municipality</label>
+                                                    <input type="text" class="form-control" name="city"
+                                                        placeholder="City/Municipality" required>
+                                                </div>
+                                                <div class="col-lg-6">
+                                                    <label class="form-label">Province</label>
+                                                    <input type="text" class="form-control" name="province"
+                                                        placeholder="Province" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <div class="row g-3">
+                                                <div class="col-lg-4">
+                                                    <label class="form-label">Course</label>
+                                                    <input type="text" class="form-control" name="course"
+                                                        placeholder="Course" required>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <label class="form-label">Year/Level</label>
+                                                    <input type="text" class="form-control" name="year_level"
+                                                        placeholder="Year/Level" required>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <label class="form-label">Section</label>
+                                                    <input type="text" class="form-control" name="section"
+                                                        placeholder="Section" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <label class="form-label">Last School Attended</label>
+                                            <textarea name="last_school" class="form-control"></textarea>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <div class="row">
+                                                <div class="col-lg-4">
+                                                    <label class="form-label">Birth Date</label>
+                                                    <input type="date" class="form-control" name="birth_date" required>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <label class="form-label">Height (ft)</label>
+                                                    <input type="text" class="form-control" name="height" required>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <label class="form-label">Weight (kg)</label>
+                                                    <input type="text" class="form-control" name="weight" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <div class="row g-3">
+                                                <div class="col-lg-3">
+                                                    <label class="form-label">Blood Type</label>
+                                                    <input type="text" class="form-control" name="blood_type"
+                                                        placeholder="Blood Type" required>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                    <label class="form-label">Gender</label>
+                                                    <select name="gender" class="form-select" required>
+                                                        <option value="">Choose...</option>
+                                                        <option>Male</option>
+                                                        <option>Female</option>
+                                                    </select>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                    <label class="form-label">Religion</label>
+                                                    <input type="text" class="form-control" name="religion"
+                                                        placeholder="Religion" required>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                    <label class="form-label">Contact No</label>
+                                                    <input type="phone" class="form-control" name="contact_no"
+                                                        placeholder="Contact No" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <div class="row g-3">
+                                                <div class="col-lg-6">
+                                                    <label class="form-label">Facebook Account</label>
+                                                    <input type="text" class="form-control" name="fb_account"
+                                                        placeholder="Facebook Account" required>
+                                                </div>
+                                                <div class="col-lg-6">
+                                                    <label class="form-label">Email Address</label>
+                                                    <input type="email" class="form-control" name="email"
+                                                        placeholder="Email Address" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <label class="form-label">Parent's Information</label>
+                                            <div class="table-responsive">
+                                                <table class="table table-bordered table-striped">
+                                                    <thead>
+                                                        <th>Surname</th>
+                                                        <th>First Name</th>
+                                                        <th>M.I.</th>
+                                                        <th>Contact No</th>
+                                                        <th>Occupation</th>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td><input type="text" class="form-control" name="surname[]"
+                                                                    placeholder="Mother's Surname"></td>
+                                                            <td><input type="text" class="form-control"
+                                                                    name="firstname[]" placeholder="Mother's Firstname">
+                                                            </td>
+                                                            <td><input type="text" class="form-control" name="mi[]"
+                                                                    placeholder="Mother's M.I."></td>
+                                                            <td><input type="text" class="form-control" name="phone[]"
+                                                                    placeholder="Mother's Contact"></td>
+                                                            <td><input type="text" class="form-control" name="work[]"
+                                                                    placeholder="Mother's Occupation"></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td><input type="text" class="form-control" name="surname[]"
+                                                                    placeholder="Father's Surname"></td>
+                                                            <td><input type="text" class="form-control"
+                                                                    name="firstname[]" placeholder="Father's Firstname">
+                                                            </td>
+                                                            <td><input type="text" class="form-control" name="mi[]"
+                                                                    placeholder="Father's M.I."></td>
+                                                            <td><input type="text" class="form-control" name="phone[]"
+                                                                    placeholder="Father's Contact"></td>
+                                                            <td><input type="text" class="form-control" name="work[]"
+                                                                    placeholder="Father's Occupation"></td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <label class="form-label">Emergency Contact Information</label>
+                                            <input type="text" class="form-control" name="emergency_contact"
+                                                placeholder="Emergency Contact" required>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <div class="row g-3">
+                                                <div class="col-lg-3">
+                                                    <label class="form-label">House No</label>
+                                                    <input type="text" class="form-control" name="house_no"
+                                                        placeholder="House No" required>
+                                                </div>
+                                                <div class="col-lg-3">
+                                                    <label class="form-label">Street</label>
+                                                    <input type="text" class="form-control" name="street"
+                                                        placeholder="Street" required>
+                                                </div>
+                                                <div class="col-lg-6">
+                                                    <label class="form-label">Barangay/Subdivision</label>
+                                                    <input type="text" class="form-control" name="barangay"
+                                                        placeholder="Barangay/Subdivision" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <div class="row g-3">
+                                                <div class="col-lg-6">
+                                                    <label class="form-label">City/Municipality</label>
+                                                    <input type="text" class="form-control" name="city"
+                                                        placeholder="City/Municipality" required>
+                                                </div>
+                                                <div class="col-lg-6">
+                                                    <label class="form-label">Province</label>
+                                                    <input type="text" class="form-control" name="province"
+                                                        placeholder="Province" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-">
+                                            <div class="row g-3">
+                                                <div class="col-lg-4">
+                                                    <label class="form-label">Relationship</label>
+                                                    <input type="text" class="form-control" name="relationship"
+                                                        placeholder="Relationship" required>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <label class="form-label">Contact No</label>
+                                                    <input type="phone" class="form-control" name="contact_no"
+                                                        placeholder="Contact No" required>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <label class="form-label">Email</label>
+                                                    <input type="text" class="form-control" name="email"
+                                                        placeholder="Email" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <button type="submit" class="btn btn-success">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                    class="icon icon-tabler icons-tabler-outline icon-tabler-device-floppy">
+                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                    <path
+                                                        d="M6 4h10l4 4v10a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2" />
+                                                    <path d="M12 14m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                                                    <path d="M14 4l0 4l-6 0l0 -4" />
+                                                </svg>
+                                                Register
+                                            </button>
+                                        </div>
                                     </form>
                                 </div>
                             </div>
